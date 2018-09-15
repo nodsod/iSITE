79907628738223c973b25b82f09ae1ce8c86b709
Kevin <kevin@Peters-MacBook-Pro.local>
2018-09-15 20:57:35 +1200

    adding bootstrap to index page

--- src/Interests.tsx
+++ src/Interests.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react';
 import './App.css';
 
+import { Col, Grid, Image, Row } from 'react-bootstrap';
+
 import mountainBiking from './kaiteriteri-mountain-bike-park.jpg';
-
-
 
 class Interests extends React.Component {
   public render() {
     return (
-        <form>
-             <img className="App-interestimage" src={mountainBiking} />
-        </form>
+        <div>
+            <Grid>
+                <Row>
+                    <Col xs={6} md={4}>
+                        <Image className="App-interestimage" src={mountainBiking} rounded={true} />
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Image className="App-interestimage" src={mountainBiking} rounded={true} />
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Image className="App-interestimage" src={mountainBiking} rounded={true} />
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Image className="App-interestimage" src={mountainBiking} rounded={true} />
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <Image className="App-interestimage" src={mountainBiking} rounded={true} />
+                    </Col>
+                </Row>
+             </Grid>
+        </div>
     );
   }
 }
